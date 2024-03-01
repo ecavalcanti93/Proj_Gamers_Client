3c5f27c2d502bc61ec86766e23e489f5d5b45740
Marcel Costa <marcelcosta@gmail.com>
2024-03-01 18:19:21 +0100

    user games in progress

---
+++ src/components/SearchProfile.jsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./SearchBar.css";
+import BasicModal from "./Modal";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+function SearchProfile() {
+  const [games, setGames] = useState([]);
+  const [searchProfileGames, setSearchProfileGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const storedToken = localStorage.getItem("authToken");
+
+  const fetchGames = () => {
+    axios
+      .get(`${API_URL}/games`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((res) => {
+        const sortedGames = res.data.sort((a, b) => {
+          return a.title.localeCompare(b.title);
+        });
+        setGames(sortedGames);
+        setSearchProfileGames(sortedGames);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching games:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
+
+  const handleSearchInputChange = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const filteredProfileGames = games.filter((game) => {
+      return game.title.toLowerCase().includes(searchTerm);
+    });
+    setSearchProfileGames(filteredProfileGames);
+  };
+
+  return (
+    <div className="center">
+      <div className="search-box flex-center">
+        <BasicModal />
+        <input
+          type="text"
+          placeholder="Search for games..."
+          onChange={handleSearchInputChange}
+          className="input-search"
+        />
+      </div>
+
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="list-container">
+          {searchProfileGames.map((game) => (
+            <div key={game._id}>
+              <Link to={`/games/${game._id}`}>
+                <img src={game.image} alt={game.title} className="list-img" />
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SearchProfile;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -15,48 +15,6 @@ function ProfilePage() {
   return (
     <div>
       <Card />
-      {/* <h1>Profile of: {user.username}</h1>
-      <img src={user.userImage} alt="profile image" />
-      <p>{user.email}</p>
-      <p>Your games</p> */}
-
-      {/* <button
-        onClick={() => {
-          handleForm();
-        }}
-      >
-        Edit Profile
-      </button>
-
-      {editForm && (
-        <form onSubmit={handleProfileSubmit}>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="name"
-            value={username}
-            onChange={handleUsername}
-          />
-
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleEmail}
-          />
-
-          <label>Profile Image:</label>
-          <input
-            type="input"
-            name="userImage"
-            value={userImage}
-            onChange={handleUserImage}
-          />
-
-          <button type="submit">Save</button>
-        </form>
-      )} */}
       <SearchBar />
     </div>
   );
