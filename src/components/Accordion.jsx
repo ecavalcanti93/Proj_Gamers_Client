9d2a785cd95e433153f14b60352003753174a93b
Marcel Costa <marcelcosta@gmail.com>
2024-03-11 15:29:31 +0100

    home accordion ok

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,5 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Accordion.css";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 import image1 from "../assets/image1.jpg";
 import image2 from "../assets/image2.jpg";
 import image3 from "../assets/image3.jpg";
@@ -10,8 +15,6 @@ import image7 from "../assets/image7.webp";
 import image8 from "../assets/image8.jpg";
 import image9 from "../assets/image9.jpg";
 import image10 from "../assets/image10.jpeg";
-
-import "./Accordion.css";
 
 const cards = [
   {
@@ -67,26 +70,118 @@ const cards = [
 ];
 
 export const Accordion = () => {
+  const [games, setGames] = useState([]);
+  const [someGames, setSomeGames] = useState([]);
+  const [gamesId, setGamesId] = useState([]);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
   const [active, setActive] = useState(0);
 
   const handleToggle = (index) => setActive(index);
 
+  const fetchGames = () => {
+    // const storedToken = localStorage.getItem("authToken");
+
+    axios
+      .get(`${API_URL}/games`)
+      .then((res) => {
+        const randomGames = res.data.sort(() => {
+          return Math.random() - 0.5;
+        });
+        return randomGames;
+
+        // someGames.length <= 10 ? someGames.push(
+        //   randomGames[0],
+        //   randomGames[1],
+        //   randomGames[2],
+        //   randomGames[3],
+        //   randomGames[4],
+        //   randomGames[5],
+        //   randomGames[6],
+        //   randomGames[7],
+        //   randomGames[8],
+        //   randomGames[9],
+        //   ) : someGames;
+        // randomGames.map((game)=>{
+        //   return someGames.push(randomGames.indexOf(game) < 10 && game)
+        // })
+        // setGames(sortedGames);
+        // setSomeGames(tenRandomGames);
+        // setLoading(false);
+      })
+      .then((res) => {
+        const tenRandomGames = [
+          res[0],
+          res[1],
+          res[2],
+          res[3],
+          res[4],
+          res[5],
+          res[6],
+          res[7],
+          res[8],
+          res[9],
+        ];
+        setSomeGames(tenRandomGames);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching games:", error);
+      });
+  };
+  // console.log(someGames);
+  useEffect(() => {
+    fetchGames();
+  }, []);
+
   return (
+    // <section className="accordions">
+    //   {cards.map((card, index) => {
+    //     const isActive = active === index ? "active" : "";
+    //     return (
+    //       <article
+    //         key={card.header}
+    //         className={isActive}
+    //         onClick={() => handleToggle(index)}
+    //       >
+    //         <img src={card.image} alt={card.header} />
+    //         <div className="content">
+    //           {/* <span className="material-symbols-outlined">+</span> */}
+    //           <div>
+    //             <h2>{card.header}</h2>
+    //             <p>{card.text}</p>
+    //           </div>
+    //         </div>
+    //       </article>
+    //     );
+    //   })}
+    // </section>
+
     <section className="accordions">
-      {cards.map((card, index) => {
+      {someGames.map((game, index) => {
         const isActive = active === index ? "active" : "";
         return (
           <article
-            key={card.header}
+            key={game.title}
             className={isActive}
-            onClick={() => handleToggle(index)}
+            onClick={() => {
+              !isActive ? (
+                handleToggle(index)
+              ) : (
+                <>
+                {/* {console.log(game._id)} */}
+                  {/* <Link to={`/games/${game._id}`}/> */}
+                  {navigate(`/games/${game._id}`)}
+                </>
+              );
+            }}
           >
-            <img src={card.image} alt={card.header} />
+            <img src={game.image} alt={game.title} />
             <div className="content">
               {/* <span className="material-symbols-outlined">+</span> */}
               <div>
-                <h2>{card.header}</h2>
-                <p>{card.text}</p>
+                <h2>{game.title}</h2>
+                <p>{game.company}</p>
               </div>
             </div>
           </article>
